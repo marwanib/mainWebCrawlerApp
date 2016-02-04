c3b4c71e0d876543bc235a64f8b55b8434539d37
marwanib <ibrahimmarwan1993@gmail.com>
2016-02-05 00:37:52 +0200

    s
    s

--- index.js
+++ index.js
@@ -23,13 +23,29 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 app.get('/db', function (request, response) {
-mongoose.connect('mongodb://Bashar:bashar15@ds039155.mongolab.com:39155/webcrawler', function(err) {
+ mongoose.connect('mongodb://Bashar:bashar15@ds039155.mongolab.com:39155/webcrawler', function(err) {
     if(err) {
         console.log('connection error', err);
     } else {
         console.log('connection successful');
-		 response.send('hello');
+		 
     }
 });
+var TodoSchema = new mongoose.Schema({
+  name: String,
+  completed: Boolean,
+  note: String,
+  updated_at: { type: Date, default: Date.now },
+});
+var Todo = mongoose.model('Todo', TodoSchema);
+Todo.create({name: 'Master Javscript', completed: true, note: 'Getting better everyday'}, function(err, todo){
+    if(err) console.log(err);
+    else console.log(todo);
+});
+ Todo.find({completed: true }, function(err, todo){
+    if(err) response.send(err);
+   else response.json(todo);
+  });
+
 });
 
