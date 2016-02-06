7e9a335187d30cc487a209fad5003549626ff2aa
marwanib <ibrahimmarwan1993@gmail.com>
2016-02-06 03:02:46 +0200

    f
    f

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
-//app.use(bodyParser.urlencoded({ extended: false }))
-//app.use(bodyParser.text())
-// parse application/json
-//app.use(bodyParser.json())
+app.use(bodyParser.text())
+ //parse application/json
+app.use(bodyParser.json())
 var mongoose = require('mongoose');
 	var db = mongoose.connection;
 	 mongoose.connect('mongodb://Bashar:bashar15@ds039155.mongolab.com:39155/webcrawler', function(err) {
@@ -21,7 +20,7 @@ var mongoose = require('mongoose');
   url: String,
   completed: Boolean,
   note: String,
-  updated_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now }
 });
 var seed = mongoose.model('seeds', TodoSchema);
 
@@ -58,7 +57,7 @@ app.get('/get_url', function (request, response) {
   });
 
 });
-/*app.post('/post_url', function (req, res) {
+app.post('/post_url', function (req, res) {
 var input_in = req.body.name;
    console.log(req.body.name);
 	res.send(input_in);
@@ -66,5 +65,5 @@ var input_in = req.body.name;
     if(err) console.log(err);
     else console.log(seed);
 });
-})*/
+});
 
