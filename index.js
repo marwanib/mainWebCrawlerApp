fbe68d417d9be59a95764dc1a145d208ae1e1896
marwanib <ibrahimmarwan1993@gmail.com>
2016-02-06 03:40:25 +0200

    d
    d

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
-//app.use(bodyParser.text())
+app.use(bodyParser.text())
  //parse application/json
 app.use(bodyParser.json())
 var mongoose = require('mongoose');
