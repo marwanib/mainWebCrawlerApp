2ff9bae1c5675b6ac4c37fe5c285b32b1f376192
marwanib <ibrahimmarwan1993@gmail.com>
2016-03-12 00:02:13 +0200

    c
    c

--- index.js
+++ index.js
@@ -104,10 +104,10 @@ app.post('/post_indexword', function (req, res) {
 input_array=req.body;
 crawledSeed.count(function(err, count){
 	if(err){ console.log(err);
-	}else {if((count%100)==0){
+	}else {
 	peformance.create({crawledPagesNumber: count }, function(err, s){
         if(err) console.log(err); });
- }}});
+ }});
 for(var i in input_array ){
 var input_url = input_array[i].url;
 var input_indexword=input_array[i].indexWord;
